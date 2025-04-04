175d0b93e071bed1a147d8aede9ace3578e666b0
Etienne <et.cassin@gmail.com>
2025-04-04 14:16:08 +0200

    formulaires reactifs

--- src/app/components/pages/inscription/inscription.component.ts
+++ src/app/components/pages/inscription/inscription.component.ts
@@ -1,29 +1,38 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UtilisateurService } from '../../../services/utilisateur.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inscription',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
 export class InscriptionComponent {
-  username = "";
-  password = "";
+  formulaire_inscription: FormGroup
 
   constructor(
     private router: Router,
     private service: UtilisateurService
-  ) {}
+  ) {
+    this.formulaire_inscription = new FormGroup({
+      username: new FormControl("", Validators.required),
+      password: new FormControl("", [Validators.required, Validators.minLength(8)])
+    });
+  }
 
   inscrire() {
-    this.service.add_utilisateur({
-      "username": this.username,
-      "password": this.password
-    }).subscribe(response => {
-      this.router.navigate(["/connexion"]);
-    });
+    if (this.formulaire_inscription.valid) {
+      this.service.add_utilisateur(this.formulaire_inscription.value).subscribe(response => {
+        this.router.navigate(["/connexion"]);
+      });
+    }
+  }
+
+  display_error(field : string, error : string) {
+    return this.formulaire_inscription?.get(field)?.dirty
+        && this.formulaire_inscription?.get(field)?.errors?.[error];
   }
 }
